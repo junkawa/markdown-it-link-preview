501f9ff4bc08e29fe8b25933dd4a580a4b7f961e
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2020-10-09 19:11:00 +0900

    Add example argument URL

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,14 @@
+let url;
+if (process.argv.length === 2) {
+  url = 'https://github.com/junkawa/figma_jp';
+} else {
+  url = process.argv[2];
+}
+
+
 const linkPreviewPlugin = require('../src/index');
 const md = require('markdown-it')().use(linkPreviewPlugin);
 const result = md.render(
-    '[@preview](https://github.com/junkawa/figma_jp)',
+    '[@preview]('+url+')',
 );
 console.log(result);
