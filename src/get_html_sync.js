b47339d0b4df75ad6fdb528e181949cd99565411
Louis Frank <singulosta@gmail.com>
2023-06-25 03:50:46 +0200

    follow redirects and 302 Found

--- src/get_html_sync.js
+++ src/get_html_sync.js
@@ -6,11 +6,16 @@ function getHtmlSync(url) {
   const xhr = new XMLHttpRequest();
   try {
     xhr.open('GET', url, false);
+    xhr.followRedirects = true;
     xhr.send();
   } catch (err) {
     throw new Error('XMLHttpRequest(' + url + ') failed. ' + err);
   }
 
+  if (xhr.readyState === 4 && (xhr.status === 301 || xhr.status === 302)) {
+    const redirectUrl = xhr.getResponseHeader('Location');
+    return getHtmlSync(redirectUrl);
+  }
   if (xhr.status != 200) {
     const statusText = xhr.statusText ? xhr.statusText : '';
     throw new Error(
